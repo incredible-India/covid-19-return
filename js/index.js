b16c6358054397c0813f504bd020bc1a3ec86330
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-04-27 23:47:26 +0530

    loader and nav bar created

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let LoaderFile  = document.getElementsByClassName('loader')[0];//loader at the starting it will show..
+
+
+
+//this is the loader when body`s document will be ready it will be removed
+
+document.body.onload = function (){
+
+    LoaderFile.style.display = "none";
+
+}
+////////////////////////////////////////
+
+
